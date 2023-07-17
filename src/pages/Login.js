34785f2211f49347b0e043bd6e275fab4735c694
Thais Kotovicz <thais_kotovicz@hotmail.com>
2023-07-17 16:01:20 -0300

    feat: creates fetch to turnover and headcount endpoints

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,41 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+// const BASE_URL = 'https://z64iardwce.execute-api.us-east-1.amazonaws.com/dev/api/v1/payroll/';
 
 function Login() {
   const [email, setEmail] = useState('');
+  const navigate = useNavigate();
 
   const handleEmailChange = ({ target }) => {
-    setEmail(target.value);
+    const { value } = target;
+    setEmail(value);
   };
 
-  const handleSearch = () => {
-    console.log('Email:', email);
+  const handleSearch = async () => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isValidEmail = emailRegex.test(email);
+
+    if (!isValidEmail) {
+      alert('Digite um email válido');
+      return;
+    }
+
+    navigate('/charts', { state: email });
+    // try {
+    //   const response = await fetch(`${BASE_URL}${email}`);
+    //   if (response.ok) {
+    //     const data = await response.json();
+
+    //     console.log(data);
+
+    //     navigate('/charts', { state: { data, email } });
+    //   } else {
+    //     alert('Erro: email não encontrado.')
+    //   }
+    // } catch (error) {
+    //   console.error(error.message);
+    // }
   };
 
   return (
