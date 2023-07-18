169fb50ed754f8c6a55149076a65aea9f9445e02
Thais Kotovicz <thais_kotovicz@hotmail.com>
2023-07-17 21:57:11 -0300

    feat: creates HeadcountChart component

---
+++ src/components/HeadcountChart.js
@@ -0,0 +1,143 @@
+import React from "react";
+import { ResponsiveLine } from "@nivo/line";
+
+
+
+
+function HeadcountChart({ data }) {
+  if (!data) return null;
+
+  const organizedData = {};
+  
+  data.headcount.forEach(({ month, count }) => {
+    const [year, monthNumber] = month.split('-');
+    if (!organizedData[year]) {
+      organizedData[year] = [];
+    }
+    organizedData[year].push({ month: monthNumber, count });
+  });
+  
+  // ordena os meses
+  Object.values(organizedData).forEach((element) => {
+    element.sort((a, b) => parseInt(a.month) - parseInt(b.month));
+  });
+  // Ordena os anos
+  const sortedData = Object.entries(organizedData).sort((a, b) => parseInt(a[0]) - parseInt(b[0]));
+
+  // Cria um novo objeto com os anos ordenados
+  const orderedData = sortedData.reduce((acc, [year, data]) => {
+    acc[year] = data;
+    return acc;
+  }, {});
+
+  const monthOrder = ["03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "01", "02"];
+  
+  const chartData = Object.entries(orderedData).map(([year, dataPoints]) => ({
+    id: year,
+    data: dataPoints
+      .map(({ month, count }) => ({
+        x: monthOrder.indexOf(month) + 1,
+        y: count,
+      }))
+      .sort((a, b) => a.x - b.x), // Ordenar os dados pelo índice
+  }));
+
+  return (
+    <div style={{ height: '400px' }}>
+      <ResponsiveLine
+        data={chartData}
+        margin={{ top: 50, right: 110, bottom: 50, left: 100 }}
+        xScale={{
+          type: 'point',
+          useUTC: false,
+          precision: 'month',
+        }}
+        yScale={{ type: "linear", min: 0, max: 40 }}
+        curve="monotoneX"
+        axisTop={null}
+        axisRight={null}
+        axisBottom={{
+          orient: "bottom",
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: 'Mês',
+          legendPosition: 'middle',
+          legendOffset: 36,
+        }}
+        axisLeft={{
+          orient: "left",
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: 'Contratações',
+          legendPosition: 'middle',
+          legendOffset: -40,
+        }}
+        enablePoints={true}
+        pointSize={12}
+        pointColor="#ffffff"
+        pointBorderWidth={2}
+        pointBorderColor={{ from: 'serieColor', modifiers: [] }}
+        enableGridX={true}
+        enableGridY={true}
+        useMesh={true}
+        colors={{ scheme: 'purpleRed_green' }}
+        // colors={{ scheme: 'spectral' }}
+        lineWidth={4}
+        enableArea={true}
+        legends={[
+          {
+            anchor: 'bottom-right',
+            direction: 'column',
+            justify: false,
+            translateX: 100,
+            translateY: 0,
+            itemsSpacing: 0,
+            itemDirection: 'left-to-right',
+            itemWidth: 80,
+            itemHeight: 20,
+            itemOpacity: 0.75,
+            symbolSize: 16,
+            symbolShape: 'circle',
+            symbolBorderColor: 'rgba(0, 0, 0, .5)',
+            effects: [
+              {
+                on: 'hover',
+                style: {
+                  itemBackground: 'rgba(0, 0, 0, .03)',
+                  itemOpacity: 1
+                }
+              }
+            ]
+          }
+        ]}
+      />
+    </div>
+  )
+}
+
+export default HeadcountChart
+
+
+  // RETORNANDO MESES DE 03 A 02
+  /*   const chartData = Object.entries(orderedData).map(([year, dataPoints]) => ({
+    id: year,
+    data: dataPoints.map(({ month, count }) => ({
+      x: month,
+      y: count,
+    })),
+  })); */
+
+  // RETORNANDO UM ANO NA SEQUÊNCIA DO OUTRO SEM SOBREPOR
+  /* const chartData = Object.entries(orderedData).map(([year, dataPoints]) => {
+    const formattedDataPoints = dataPoints.map(({ month, count }) => ({
+      x: `${year}-${month}`,
+      y: count,
+    }));
+
+    return {
+      id: year,
+      data: formattedDataPoints.sort((a, b) => monthOrder.indexOf(a.x.split('-')[1]) - monthOrder.indexOf(b.x.split('-')[1])),
+    };
+  }); */
--- src/pages/Charts.js
+++ src/pages/Charts.js
@@ -1,25 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import HeadcountChart from '../components/HeadcountChart';
 
 function Charts() {
-  const [headcount, setHeadCount] = useState([]);
-  const [turnover, setTurnover] = useState([]);
+  const [headcount, setHeadCount] = useState(null);
+  const [turnover, setTurnover] = useState(null);
   const location = useLocation();
   const email = location.state;
 
   // const BASE_URL = 'https://z64iardwce.execute-api.us-east-1.amazonaws.com/dev/api/v1/payroll/';
 
   useEffect(() => {
-    // fetchHeadcount();
-    // fetchTurnover();
     try {
       fetchHeadcount();
       fetchTurnover();
     } catch (error) {
       console.error('Ocorreu um erro: ', error);
     }
-    console.log('TURNOVER', turnover);
-    console.log('HEADCOUNT', headcount);
   }, []);
 
   const fetchHeadcount = async () => {
@@ -28,172 +25,172 @@ function Charts() {
     const data = {
       headcount: [
         {
-            "month": "2021-03",
-            "count": 17
+            month: "2021-03",
+            count: 17
         },
         {
-            "month": "2021-04",
-            "count": 18
+            month: "2021-04",
+            count: 18
         },
         {
-            "month": "2021-05",
-            "count": 19
+            month: "2021-05",
+            count: 19
         },
         {
-            "month": "2021-06",
-            "count": 21
+            month: "2021-06",
+            count: 21
         },
         {
-            "month": "2021-07",
-            "count": 24
+            month: "2021-07",
+            count: 24
         },
         {
-            "month": "2021-08",
-            "count": 22
+            month: "2021-08",
+            count: 22
         },
         {
-            "month": "2021-09",
-            "count": 25
+            month: "2021-09",
+            count: 25
         },
         {
-            "month": "2021-10",
-            "count": 23
+            month: "2021-10",
+            count: 23
         },
         {
-            "month": "2021-11",
-            "count": 23
+            month: "2021-11",
+            count: 23
         },
         {
-            "month": "2021-12",
-            "count": 24
+            month: "2021-12",
+            count: 24
         },
         {
-            "month": "2022-01",
-            "count": 23
+            month: "2022-01",
+            count: 23
         },
         {
-            "month": "2022-02",
-            "count": 23
+            month: "2022-02",
+            count: 23
         },
         {
-            "month": "2022-03",
-            "count": 23
+            month: "2022-03",
+            count: 23
         },
         {
-            "month": "2022-04",
-            "count": 23
+            month: "2022-04",
+            count: 23
         },
         {
-            "month": "2022-05",
-            "count": 24
+            month: "2022-05",
+            count: 24
         },
         {
-            "month": "2022-06",
-            "count": 25
+            month: "2022-06",
+            count: 25
         },
         {
-            "month": "2022-07",
-            "count": 27
+            month: "2022-07",
+            count: 27
         },
         {
-            "month": "2022-08",
-            "count": 28
+            month: "2022-08",
+            count: 28
         },
         {
-            "month": "2022-09",
-            "count": 30
+            month: "2022-09",
+            count: 30
         },
         {
-            "month": "2022-10",
-            "count": 30
+            month: "2022-10",
+            count: 30
         },
         {
-            "month": "2022-11",
-            "count": 30
+            month: "2022-11",
+            count: 30
         },
         {
-            "month": "2022-12",
-            "count": 30
+            month: "2022-12",
+            count: 30
         },
         {
-            "month": "2023-01",
-            "count": 30
+            month: "2023-01",
+            count: 30
         },
         {
-            "month": "2023-02",
-            "count": 30
+            month: "2023-02",
+            count: 30
         },
         {
-            "month": "2023-03",
-            "count": 30
+            month: "2023-03",
+            count: 30
         },
         {
-            "month": "2023-04",
-            "count": 30
+            month: "2023-04",
+            count: 30
         },
         {
-            "month": "2023-05",
-            "count": 30
+            month: "2023-05",
+            count: 30
         },
         {
-            "month": "2023-06",
-            "count": 30
+            month: "2023-06",
+            count: 30
         },
         {
-            "month": "2023-07",
-            "count": 30
+            month: "2023-07",
+            count: 30
         },
         {
-            "month": "2020-08",
-            "count": 5
+            month: "2020-08",
+            count: 5
         },
         {
-            "month": "2020-09",
-            "count": 7
+            month: "2020-09",
+            count: 7
         },
         {
-            "month": "2020-10",
-            "count": 8
+            month: "2020-10",
+            count: 8
         },
         {
-            "month": "2020-11",
-            "count": 8
+            month: "2020-11",
+            count: 8
         },
         {
-            "month": "2020-12",
-            "count": 10
+            month: "2020-12",
+            count: 10
         },
         {
-            "month": "2021-01",
-            "count": 10
+            month: "2021-01",
+            count: 10
         },
         {
-            "month": "2021-02",
-            "count": 13
+            month: "2021-02",
+            count: 13
         },
         {
-            "month": "2020-03",
-            "count": 1
+            month: "2020-03",
+            count: 1
         },
         {
-            "month": "2020-04",
-            "count": 2
+            month: "2020-04",
+            count: 2
         },
         {
-            "month": "2020-05",
-            "count": 2
+            month: "2020-05",
+            count: 2
         },
         {
-            "month": "2020-06",
-            "count": 3
+            month: "2020-06",
+            count: 3
         },
         {
-            "month": "2020-07",
-            "count": 3
+            month: "2020-07",
+            count: 3
         }
       ]
     }
-    setHeadCount(data.headcount);
+    setHeadCount(data);
   }
 
   const fetchTurnover = async () => {
@@ -244,24 +241,17 @@ function Charts() {
         {month: '2020-07', value: 0},
       ]
     }
-    setTurnover(data.turnover);
+    setTurnover(data);
   }
 
   return (
     <div>
       <h1>Payroll Charts</h1>
-
       <h2>Headcount</h2>
 
-      { headcount.map((elem, index) => (
-          <p key={index}>{elem.month}: {elem.count}</p>
-      )) }
+      <HeadcountChart data={ headcount } />
 
       <h2>Turnover</h2>
-      
-      { turnover.map((elem, index) => (
-          <p key={index}>{elem.month}: {elem.value}</p>
-      )) }
     </div>
   );
 }
