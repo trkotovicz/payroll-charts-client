bc939d68f941a5b90bf2575bbceabfcd6b62642e
Thais Kotovicz <thais_kotovicz@hotmail.com>
2023-07-18 15:36:00 -0300

    style: add MUI to home page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+
+// const theme = createTheme({
+//   palette: {
+//     primary: {
+//       light: "#42a5f5",
+//       main: "#1976d2",
+//       dark: "#1565c0",
+//     },
+//     secondary: {
+//       light: "#ba68c8",
+//       main: "#9c27b0",
+//       dark: "#7b1fa2",
+//     },
+//     error: {
+//       light: "#ef5350",
+//       main: "#d32f2f",
+//       dark: "#c62828",
+//     }
+//   },
+//   typography: {
+//     h1: {
+//       fontSize: "3rem",
+//       fontWeight: 600,
+//     },
+//     h2: {
+//       fontSize: "1.75rem",
+//       fontWeight: 600,
+//     },
+//     h3: {
+//       fontSize: "1.5rem",
+//       fontWeight: 600,
+//     },
+//   },
+// });
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#4285F4',
+    },
+    background: {
+      default: '#fff',
+    },
+  },
+  typography: {
+    fontFamily: 'Google Sans, Roboto, Arial, sans-serif',
+  },
+});
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={ theme }>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,5 @@
+import { Button, Container, Icon, Paper, TextField, Typography } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -15,28 +17,46 @@ function Home() {
       setEmail('');
       return;
     }
-  
     navigate('/charts', { state: { email } });
   };
 
   return (
-    <div>
-      <h1>Payroll Charts</h1>
+     <Container sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
+      <Typography variant='h1' sx={{ my: 4, color: 'primary.main', textAlign: 'center' }}>
+        Payroll Charts
+      </Typography>
 
-      {showHidden ? (
-        <span className="span-email-invalid">
-          Digite um email válido
-        </span>
-      ) : ''}
+      <Paper elevation={4} sx={{ p: 8, my: 8, width: '80%', background: '#f8f9fa', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          {showHidden ? (
+            <TextField
+              error
+              id="standard-error-helper-text"
+              type="email"
+              label="Email"
+              helperText="Digite o email para busca"
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+              sx={{ flex: '0.7', m: 2 }}
+            />
+          ) : (
+            <TextField
+              variant="outlined"
+              type="email"
+              label="Digite o email para busca"
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+              sx={{ flex: '0.7', m: 2 }}
+            />
+          )}
 
-      <input
-        type="email"
-        placeholder="Digite seu email"
-        value={ email }
-        onChange={ ({ target }) => setEmail(target.value) }
-      />
-      <button onClick={ handleSearch }>Buscar</button>
-    </div>
+          <Button variant='contained' sx={{ m: 2, p: 2, bgcolor: 'primary.light', textTransform: 'none', fontWeight: 'bold', display: 'flex', alignItems: 'center', justifyContent: 'center', gap: 1 }} onClick={handleSearch}>
+            <Icon>
+              <SearchIcon />
+            </Icon>
+          </Button>
+        </Paper>
+
+    </Container>
   );
 }
 
